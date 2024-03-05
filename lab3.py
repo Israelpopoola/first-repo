7955f44909ad20d6f1a7ee24a383e5187f9fa40e
Israel Popoola <israelpopoola@Israels-Air.attlocal.net>
2024-03-05 14:52:01 -0600

    done with the second lab and made another variation with user input integration

--- lab3.py
+++ lab3.py
@@ -2,39 +2,65 @@
 
 # Create a function to calculate the percentage of a number and return it.
 #
+
+
+# numerator_insert = int(input("Insert numerator: "))
+#
+# denominator_insert = int(input("Insert denominator: "))
+#
+# def calculate_percentage( numerator_insert, denominator_insert):
+#    if denominator_insert == 0:
+#      raise (ZeroDivisionError("CANNOT CALCULATE PERCENTAGE WITH ZERO AS DENOMINATOR."))
+#    'raise is for errors'
+#
+#    return (numerator_insert / denominator_insert ) *100
+#
+#
+# percentage = calculate_percentage (numerator_insert, denominator_insert)
+# print(f"{percentage:.2f}%")
+
+
+
+
+
+
 # Create a function that takes in a list and returns the last item in the list
 #
+
+# list = [1,2,3,4,5,6,7,8,9,10]
+#
+# def get_last(list):
+#   return list [-1]
+#
+# print(get_last(list))
+# print(dir(list_last_in_list))
+
+'''I now want to make it so that its more "user input" integrated'''
+
+
+#
+# input_list = list(input("insert numbers or letters seperated by ',': " ))
+#
+# space_input_list = list(input_list)
+
+# def get_last(space_input_list):
+#    return space_input_list [-1]
+#
+# print(get_last(space_input_list))
+
+
 # Create a function that takes in a dictionary and returns the value of the second item
 #
+
+
+
+
+
+
+
+
+
 # Create a function that adds two lists together and returns the value
 
 
-numerator_insert = int(input("Insert numerator: "))
 
-denominator_insert = int(input("Insert denominator: "))
-
-def calculate_percentage( numerator_insert, denominator_insert):
-  if denominator_insert == 0:
-    raise ZeroDivisionError("Cannot calculate percentage with a zero denominator.")
-
-  return (numerator_insert / denominator_insert ) *100
-
-
-percentage = calculate_percentage (numerator_insert, denominator_insert)
-print(f"{percentage:.2f}%")
-
-
-
-
-
-
-"""
- This function calculates the percentage of a numerator relative to a denominator.
-
- Args:
-     numerator: The value to be expressed as a percentage.
-     denominator: The total value against which the percentage is calculated.
-
- Returns:
-     The percentage value as a float.
- """
